a936387d0bb41d8378494abb44792dc926ad3dde
susanna <susannae.repo@gmail.com>
2021-11-12 19:48:35 +0200

    add docstring to individual.py

--- src/individual.py
+++ src/individual.py
@@ -1,12 +1,42 @@
 from matplotlib import pyplot as plot
 import numpy as np
+    """Class for a person and their images
+
+    Raises:
+        ValueError: if needed
+
+    Returns nothing
+    """
 
 class Individual:
+    """
+    Class for one individual and their images as column vectors
+    in matrix images_set
+    """
     def __init__(self, im_set_matrix):
+        """Constructor
+
+        Args:
+            im_set_matrix (np.array): np.array containing images
+            of the person as np.arrays
+        """
         self.images_set = Individual.matrix3d_submatrices_to_colums(im_set_matrix)
 
     @staticmethod
     def matrix3d_submatrices_to_colums(im_set_matrix):
+        """
+        Static function to turn n*n image np.arrays in an np.array
+        to column vectors of length n*n
+
+        Args:
+            im_set_matrix (np.array): 3-dim np.array containing images as np.array
+
+        Raises:
+            ValueError: if arg is not a 3-dim np.array
+
+        Returns:
+            np.array: array containing images as column vectors
+        """
         if im_set_matrix.ndim != 3:
             raise ValueError("invalid shape: " + str(im_set_matrix.shape))
         (count, row, col) = im_set_matrix.shape
@@ -19,9 +49,20 @@ class Individual:
 
     @staticmethod
     def matrix2d_to_column_vector(im_matrix):
+        """Static function to turn 2-dim np.arrays to a vector
+
+        Args:
+            im_matrix (np.array): 2-dim np.array
+
+        Returns:
+            np.array: given arg as a column vector
+        """
         return np.array([im_matrix.flatten()]).T
 
     def show_images(self):
+        """
+        Function to plot images
+        """
         fig = plot.figure(figsize=(5, 5))
         columns = 5
         rows = 2
