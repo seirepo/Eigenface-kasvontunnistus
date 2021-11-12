0a4798036a36e8747d78e5db2a26c1d930e50a39
susanna <susannae.repo@gmail.com>
2021-11-12 14:10:24 +0200

    implement method to show images

--- src/individual.py
+++ src/individual.py
@@ -21,13 +21,24 @@ class Individual:
     def matrix2d_to_column_vector(im_matrix):
         return np.array([im_matrix.flatten()]).T
 
-    """
-    def show(self):
+    def show_images(self):
+        fig = plot.figure(figsize=(5, 5))
+        columns = 5
+        rows = 2
+        print(self.images_set[:,0])
+        for i in range(self.images_set.shape[1]):
+            #print(i)
+            fig.add_subplot(rows, columns, i+1)
+            im = self.images_set[:,i]
+            im_r = im.reshape((64, 64))
+            plot.imshow(im_r, cmap="Greys_r")
+        plot.show()
+        """
         fig = plot.figure(figsize=(5, 5))
         columns = 5
         rows = 2
         for i in range(1, columns*rows +1):
             fig.add_subplot(rows, columns, i)
-            plot.imshow(self._im_matrix_array[i-1], cmap="Greys_r")
+            plot.imshow(self.im_matrix_array[i-1], cmap="Greys_r")
         plot.show()
         """
