9d0c133fef1cb1bb389a51b2e49ec646016d8ce5
susanna <susannae.repo@gmail.com>
2021-11-18 20:47:42 +0200

    edit & add tests

--- src/tests/operations_test.py
+++ src/tests/operations_test.py
@@ -5,24 +5,50 @@ import operations as op
 class TestOperations(unittest.TestCase):
     def setUp(self):
         self.mat1 = np.array( [[ 1, 2, 3],
-                            [ 4, 5, 6]])
+                               [ 4, 5, 6]])
         self.mat2 = np.array( [[ 7, 8, 9],
-                            [ 10, 11, 12]])
+                              [ 10, 11, 12]])
         self.mat3 = np.array( [[ 13, 14, 15],
-                            [ 16, 17, 18]])
+                               [ 16, 17, 18]])
         self.mat4 = np.array( [[ 19, 20],
-                            [ 21, 22]])
+                               [ 21, 22]])
         self.mat5 = np.array([[1]])
+        self.test_matrix = np.array([ self.mat1, self.mat2, self.mat3 ])
 
     def test_matrix_submatrices_to_columns_returns_matrix_with_correct_size(self):
-        test_matrix = np.array([ self.mat1, self.mat2, self.mat3 ])
-        result = op.matrix3d_submatrices_to_columns(test_matrix)
+        result = op.matrix3d_submatrices_to_columns(self.test_matrix)
         self.assertEqual(result.shape, (6, 3))
 
     def test_matrix_submatrices_to_columns_returns_matrix_with_submatrices_as_columns(self):
-        test_matrix = np.array([ self.mat1, self.mat2, self.mat3 ])
-        result = op.matrix3d_submatrices_to_columns(test_matrix)
+        result = op.matrix3d_submatrices_to_columns(self.test_matrix)
         should_be = np.array([[1,2,3,4,5,6],[7,8,9,10,11,12],[13,14,15,16,17,18]]).T
 
         self.assertEqual(result.shape, should_be.shape)
         self.assertTrue((result == should_be).all())
+
+    def test_matrix_submatrices_to_columns_raises_error(self):
+        self.assertRaises(ValueError,
+            op.matrix3d_submatrices_to_columns, self.mat1)
+        self.assertRaises(ValueError,
+            op.matrix3d_submatrices_to_columns, np.array([self.test_matrix]))
+
+    def test_calculate_eigenface_returns_eigenface_matrix_of_correct_shape(self):
+        result1 = op.calculate_eigenfaces(self.mat1)
+        result2 = op.calculate_eigenfaces(self.mat1, 1)
+        result3 = op.calculate_eigenfaces(self.mat1, 0)
+        result4 = op.calculate_eigenfaces(self.mat4, 2)
+        result5 = op.calculate_eigenfaces(self.mat5)
+        result6 = op.calculate_eigenfaces(self.mat5, -5)
+
+        self.assertEqual(result1.shape, (2, 3))
+        self.assertEqual(result2.shape, (2, 1))
+        self.assertEqual(result3.shape, (2, 3))
+        self.assertEqual(result4.shape, (2, 2))
+        self.assertEqual(result5.shape, (1, 1))
+        self.assertEqual(result6.shape, result5.shape)
+
+    def test_calculate_eigenfaces_throws_exception(self):
+        self.assertRaises(Exception,
+            op.calculate_eigenfaces, self.mat1, 15)
+        self.assertRaises(Exception,
+            op.calculate_eigenfaces, self.mat4, 3)
