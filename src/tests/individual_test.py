de23400336da2de9796e8e11718f798d6c72b301
susanna <susannae.repo@gmail.com>
2021-11-18 21:02:06 +0200

    add test for constructor

--- src/tests/individual_test.py
+++ src/tests/individual_test.py
@@ -1,7 +1,20 @@
 import unittest
 import numpy as np
+from individual import Individual
 import operations as op
 
 class TestIndividual(unittest.TestCase):
     def setUp(self):
-        pass
+        self.images = np.array( [[ 1, 2, 3],
+                                 [ 1, 1, 1],
+                                 [ 2, 0, 1]])
+        self.eigenfaces = np.array( [[ 1, 1, 1],
+                                     [ 4, 0, 6],
+                                     [ 0, 3, 2]])
+
+    def test_constructor_creates_individual_correctly(self):
+        ind = Individual(self.images, self.eigenfaces, 2)
+
+        self.assertTrue((ind.images_set == self.images).all())
+        self.assertTrue((ind.eigenfaces == self.eigenfaces).all())
+        self.assertEqual(ind.id, 2)
