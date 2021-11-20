2a308078535b4c0be27b8d7ef964ee5ab12dd0a0
susanna <susannae.repo@gmail.com>
2021-11-20 16:47:53 +0200

    remove calculate_eigenfaces from individual

--- src/individual.py
+++ src/individual.py
@@ -25,18 +25,3 @@ class Individual:
 
     def get_test_images(self):
         return self.test_images
-
-    # for testing only
-    def calculate_eigenfaces(self):
-        average_face = np.mean(self.images_set, axis=1).reshape((-1, 1))
-        faces_minus_average = np.subtract(self.images_set, average_face)
-
-        L = np.matmul(faces_minus_average.T, faces_minus_average)
-        v = np.linalg.eig(L)[1]
-        eigenfaces = np.zeros((4096, 10))
-        M = 10
-        for i in range(0, M):
-            for j in range(0, M):
-                eigenfaces[:,i] += v[i][j] * faces_minus_average[:,j]
-
-        return eigenfaces
