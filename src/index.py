b4832af4fb78f15311e171c09123b5ad6f8f657c
susanna <susannae.repo@gmail.com>
2021-11-24 14:33:33 +0200

    random tests

--- src/index.py
+++ src/index.py
@@ -34,7 +34,28 @@ def main():
 
     # laskeaan training-setistä keskiarvo ja eigenfacet
     average_face = op.get_average_face(training_images)
-    eigenfaces = op.calculate_eigenfaces(training_images)
+    eigenfaces = op.calculate_eigenfaces(training_images, 5)
+    print(eigenfaces[:10,:3])
+    show_images(eigenfaces)
+
+    # testi form_eigenfaces varten
+    rows, cols = eigenfaces.shape
+    print(rows, cols) # 4096, 5
+    print(training_images.shape) # 4096, 320
+    # M = 320
+
+    result = np.zeros((rows, cols))
+    print(result.shape) # 4096, 5
+
+    temp = np.zeros((rows, 1))
+    #for j in range(0,):
+    #    temp += eigenfaces[:,0][j] * training_images[:,j]
+
+
+    #eigvecs = np.array([[2,1,1],[3,0,1],[2,3,2]])
+    #images = np.array([[1,2,3],[1,5,6],[0,4,2]])
+    #print(op.get_eigenfaces(images,eigvecs))
+
 
 
 def show_images(images):
