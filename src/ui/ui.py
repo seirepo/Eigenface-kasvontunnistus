17974b952095bcce217060082443d72af84bfc06
susanna <susannae.repo@gmail.com>
2021-12-02 20:19:18 +0200

    clean code

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -18,25 +18,12 @@ class UI:
             text="laske eigenfacet",
             command=self.handle_button_click
         )
-
-        button_show = ttk.Button(
-            master=self.root,
-            text="näytä kuva",
-            command=self.show_images
-        )
         
         self.canvas = tkinter.Canvas(master=self.root, width=300, height=300)
 
-        im = self.app.get_random_image()
-        img = ImageTk.PhotoImage(image=Image.fromarray(im))
-        panel = ttk.Label(self.root, image = img)
-        panel.image = img
-
         label.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
         button_calc.grid(row=1, column=0, padx=5, pady=5)
-        button_show.grid(row=1, column=1, padx=5, pady=5)
         self.canvas.grid(row=2, column=4, padx=5, pady=5)
-        panel.grid(row=2, column=3, padx=5, pady=5)
 
     def handle_button_click(self):
         print("lasketaan...")
@@ -44,8 +31,3 @@ class UI:
 
     def show_images(self):
         pass
-        #print("näytetään kuva")
-        #im = self.app.get_random_image()
-        #image = ImageTk.PhotoImage(image=Image.fromarray(im))
-        #self.im.append(image)
-        #self.canvas.create_image(20, 20, anchor="nw", image=image)
