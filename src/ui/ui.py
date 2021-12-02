4148afa3c8a0be506f8ff40e502a16972a13dcb6
susanna <susannae.repo@gmail.com>
2021-12-02 21:23:56 +0200

    one image/person is now shown on the left

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -7,23 +7,40 @@ class UI:
     def __init__(self, root, app):
         self.root = root
         self.app = app
-        self.canvas = None
-        self.im = []
+        self.left_pane = None
+        self.people = []
 
     def start(self):
-        label = ttk.Label(master=self.root, text="Testikäyttöliittymä")
-        
+
+
+        self.left_pane = ttk.Frame(master=self.root, width=280)
+        self.left_pane.pack(anchor="sw")
+        label_people = ttk.Label(master=self.left_pane, text="People")
+        label_people.grid(row=0, column=0, columnspan=4)
+
+        self.app.create_individuals()
+        ppl = self.app.get_images_of_people()
+        x = 1
+        y = 0
+        im = 1
+        for person in ppl:
+            image = ImageTk.PhotoImage(image=Image.fromarray(person))
+            self.people.append(image)
+            label = ttk.Label(self.left_pane, image=image, text=str(im), compound="top")
+            label.image = image
+            label.grid(row=x, column=y)
+            x += 1
+            if x % 11 == 0:
+                x = 1
+                y += 1
+            im += 1
+
         button_calc = ttk.Button(
-            master=self.root,
+            master=self.left_pane,
             text="laske eigenfacet",
             command=self.handle_button_click
         )
-        
-        self.canvas = tkinter.Canvas(master=self.root, width=300, height=300)
-
-        label.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
-        button_calc.grid(row=1, column=0, padx=5, pady=5)
-        self.canvas.grid(row=2, column=4, padx=5, pady=5)
+        button_calc.grid(row=11, column=0, columnspan=4)
 
     def handle_button_click(self):
         print("lasketaan...")
