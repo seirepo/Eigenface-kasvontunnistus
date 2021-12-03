6b018c15f0eed8df240da6a30802d66680ed34e8
susanna <susannae.repo@gmail.com>
2021-12-03 19:33:05 +0200

    add method to select average images for individual + test it

--- src/individual.py
+++ src/individual.py
@@ -1,3 +1,6 @@
+import operations as op
+import numpy as np
+
 class Individual:
     """
     Class for one individual and their images as column vectors
@@ -13,6 +16,7 @@ class Individual:
         self.images = images
         self.training_images, self.test_images = self.train_test_split()
         self.id = id
+        self.average_images = self.select_average()
         self.projected_images = None
 
     def get_images(self):
@@ -23,6 +27,19 @@ class Individual:
 
     def train_test_split(self):
         return (self.images[:,:8], self.images[:,8:])
+
+    def select_average(self):
+        if self.training_images.shape[1] < 2:
+            return np.array(self.training_images)
+
+        count = self.training_images.shape[1]
+        averages_list = []
+
+        for i in range(0, count, 2):
+            average = op.get_average_face(self.training_images[:,i:i+2]).flatten()
+            averages_list.append(average)
+        averages = np.array(averages_list).T
+        return averages
 
     def get_training_images(self):
         return self.training_images
--- src/tests/individual_test.py
+++ src/tests/individual_test.py
@@ -7,9 +7,21 @@ class TestIndividual(unittest.TestCase):
     def setUp(self):
         self.images = np.array( [[ 1, 2, 3],
                                  [ 1, 1, 1],
-                                 [ 2, 0, 1]])
+                                 [ 2, 0, 1]] )
 
     def test_constructor_creates_individual_correctly(self):
         ind = Individual(2, self.images)
         self.assertTrue((ind.images == self.images).all())
         self.assertEqual(ind.id, 2)
+
+    def test_select_average_returns_correct_shape(self):
+        ind = Individual(2, self.images)
+        self.assertEqual(ind.average_images.shape, (3, 2))
+
+    def test_select_average_returns_correct_array(self):
+        ims = np.hstack((self.images, np.array([1,1,1]).reshape((3,1))))
+        ind = Individual(2, ims)
+        result = ind.average_images
+        should_be = np.array([[1.5, 1, 1], [2, 1, 1]]).T
+
+        self.assertTrue((result == should_be).all())
