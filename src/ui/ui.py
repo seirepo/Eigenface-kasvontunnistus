0c126574ce7651720ef1f064aedee2f544cf08bb
susanna <susannae.repo@gmail.com>
2021-12-03 15:49:50 +0200

    somehow manage to show test images

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -44,8 +44,37 @@ class UI:
 
         self.middle_pane = ttk.Frame(master=self.root, width=280)
         self.middle_pane.grid(row=0, column=1)
-        test_label = ttk.Label(master=self.middle_pane, text="TESTI")
-        test_label.grid(row=0, column=0)
+        test_label = ttk.Label(master=self.middle_pane, text="Testikuvat")
+        test_label.grid(row=0, column=0, columnspan=5)
+
+        ppl = self.app.get_individuals()
+        #ind = ppl[0]
+        #ims = ind.get_test_images()
+        #tims = ims[:,0].reshape(64,64)
+        #im = np.uint8(tims*255)
+        #image = ImageTk.PhotoImage(image=Image.fromarray(im))
+        #radiob = ttk.Radiobutton(master=self.middle_pane, image=image, text=str(id), compound="top")
+        #radiob.image = image
+        #radiob.grid(row=1, column=0)
+        x = 1
+        y = 0
+        for individual in ppl:
+            test_images = individual.get_test_images()
+            id = individual.get_id()
+            for test_image in test_images.T:
+                test = test_image.reshape((64,64))
+                image = np.uint8(test*255)
+                image = ImageTk.PhotoImage(image=Image.fromarray(image))
+                radiob = ttk.Button(
+                    master=self.middle_pane,
+                    image=image, text=str(id), compound="top"
+                )
+                radiob.image = image
+                radiob.grid(row=x, column=y)
+                x += 1
+                if x % 11 == 0:
+                    x = 1
+                    y += 1
 
     def handle_button_click(self):
         print("lasketaan...")
