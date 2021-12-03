53fb4c2d947f4dcd504b837d335dce849919a12e
susanna <susannae.repo@gmail.com>
2021-12-03 15:25:37 +0200

    add another test, edit the other

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -5,22 +5,44 @@ from app import App
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-        pass
+        self.app = Mock()
 
     def test_get_training_images_returns_matrix_correct_shape(self):
-        individual = Mock()
-        individual.get_training_images.return_value = np.arange(0,8).reshape((-1,2))
-        individual.get_test_images.return_value = np.arange(0,4).reshape((2,2))
         #with patch.object(App, 'load_data') as mock_load:
         #    mock_load.return_value = [1, 2, 3]
         #    app = App()
         #    print(app.data)
         #    assert 0
-            
-        app = Mock()
-        app.individuals = [individual, individual]
-        training, test = App.get_training_test_images(app)
 
-        self.assertEqual(training.shape, (4, 4))
+        individual = Mock()
+        individual.get_training_images.return_value = np.arange(0,8).reshape((-1,2))
+        individual.get_test_images.return_value = np.arange(0,4).reshape((2,2))
+        self.app.individuals = [individual, individual]
+        train, test = App.get_training_test_images(self.app)
+
+        self.assertEqual(train.shape, (4, 4))
         self.assertEqual(test.shape, (2, 4))
+
+    def test_get_training_images_returns_correct_arrays(self):
+        ind1 = Mock()
+        ind2 = Mock()
+
+        a1 = np.arange(0,12).reshape((3,4))
+        a2 = np.arange(12,24).reshape((3,4))
+        b1 = np.arange(0,8).reshape((2,4))
+        b2 = np.arange(8,16).reshape((2,4))
+
+        ind1.get_training_images.return_value = a1
+        ind1.get_test_images.return_value = b1
+        ind2.get_training_images.return_value = a2
+        ind2.get_test_images.return_value = b2
+
+        self.app.individuals = [ind1, ind2]
+        train_should_be = np.hstack((a1, a2))
+        test_should_be = np.hstack((b1, b2))
+
+        train, test = App.get_training_test_images(self.app)
+
+        self.assertTrue((train == train_should_be).all())
+        self.assertTrue((test == test_should_be).all())
 
