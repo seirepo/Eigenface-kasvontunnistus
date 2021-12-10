49af4a0bb8a05f0f0d40bda9135213613d414d45
susanna <susannae.repo@gmail.com>
2021-12-10 17:30:50 +0200

    remove comments

--- src/operations.py
+++ src/operations.py
@@ -92,17 +92,17 @@ def get_most_frequent(values):
     result = zip(counts, vals)
     result = list(result)
     sorted_res = sorted(result)[::-1]
-    print("alkup lista: ", values)
-    print("järjestetty: ", sorted_res)
+    #print("alkup lista: ", values)
+    #print("järjestetty: ", sorted_res)
     max_count = sorted_res[0][0]
     max_pair = sorted_res[0]
-    print("suurin count: ", max_count, "\nsuurin: ", max_pair)
+    #print("suurin count: ", max_count, "\nsuurin: ", max_pair)
 
     tie = []
     for item in sorted_res[1:]:
         if item[0] == max_count:
             tie.append(item)
-    print("tasa: ", tie, "\n")
+    #print("tasa: ", tie, "\n")
 
     if len(tie) == 0:
         return max_pair[1]
