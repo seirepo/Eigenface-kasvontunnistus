0e8d1efa604551716d3797f38c9dc57f74170644
susanna <susannae.repo@gmail.com>
2021-12-10 22:55:11 +0200

    working messy ui

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,5 +1,6 @@
 from tkinter import Frame, Scrollbar, ttk
 import tkinter
+
 from app import App
 from PIL import Image, ImageTk
 import numpy as np
@@ -12,10 +13,8 @@ class UI:
         self.people = []
 
     def start(self):
-        #self.root.grid_columnconfigure(1, weight=1)
-        #self.root.grid_columnconfigure(0, weight=1)
         self.people_frame = ttk.Frame(master=self.root)#, width=280)
-        self.people_frame.grid(row=0, column=0, padx=50, sticky="nw")
+        self.people_frame.grid(row=0, column=0, padx=30, sticky="nw")
 
         label_people = ttk.Label(master=self.people_frame, text="Henkilöt")
         label_people.grid(row=0, column=0, columnspan=4)
@@ -42,7 +41,7 @@ class UI:
         self.test_im_frame.grid(row=0, column=0, sticky="nw")
 
         test_label = ttk.Label(master=self.middle_canvas, text="Testikuvat")
-        test_label.grid(row=0, column=0, columnspan=5)
+        test_label.grid(row=0, column=0, columnspan=16)
 
         ppl = self.app.get_individuals()
 
@@ -73,48 +72,46 @@ class UI:
         tmp_canvas_row = 1
         for ind in ppl:
             tmp_canvas = tkinter.Canvas(master=self.middle_canvas)
-            tmp_canvas.grid(row=tmp_canvas_row, column=tmp_canvas_col, padx=15)
+            tmp_canvas.grid(row=tmp_canvas_row, column=tmp_canvas_col, padx=10, pady=5)
             nearest_nbrs = ind.get_nearest_neighbor()
-            # pitää käsitellä molemmat nearest_nbrs listan alkiot!
-            test_item = nearest_nbrs[0]
-            im = test_item[0].reshape((64,64))
-            nearest_id = test_item[1]
-            im = np.uint8(im*255)
-            im = ImageTk.PhotoImage(image=Image.fromarray(im))
-            txt = "image of\n" + str(ind.get_id())
-            label = ttk.Label(
-                master=tmp_canvas,
-                image=im,
-                #text=str(ind.get_id()),
-                text=txt,
-                compound="top"
-            )
-            label.image = im
-            label.grid(row=0, column=0, padx=5)
-
-            nearest_im = self.app.get_image_by_id(nearest_id)
-            nearest_im = nearest_im.reshape((64,64))
-            nearest_im = np.uint8(nearest_im*255)
-            nearest_im = ImageTk.PhotoImage(image=Image.fromarray(nearest_im))
-            txt1 = "identified as\n" + str(nearest_id)
-            label_nearest = ttk.Label(
-                master=tmp_canvas,
-                image=nearest_im,
-                text=txt1,
-                compound="top"
-            )
-            label_nearest.image = nearest_im
-            label_nearest.grid(row=0, column=1)
-
-            tmp_canvas_row += 1
-            if tmp_canvas_row % 9 == 0:
+            row = 0
+            col = 0
+            for neighbor in nearest_nbrs:
+                print("käsittelyssä ", ind.get_id(), ": ", neighbor[1], neighbor[2])
+                im = neighbor[0].reshape((64,64))
+                nearest_id = neighbor[1]
+                im = np.uint8(im*255)
+                im = ImageTk.PhotoImage(image=Image.fromarray(im))
+                txt = "input: " + str(ind.get_id())
+                label = ttk.Label(
+                    master=tmp_canvas,
+                    image=im,
+                    text=txt,
+                    compound="top"
+                )
+                label.image = im
+                label.grid(row=row, column=col, padx=5)
+                nearest_im = self.app.get_image_by_id(nearest_id)
+                nearest_im = nearest_im.reshape((64,64))
+                nearest_im = np.uint8(nearest_im*255)
+                nearest_im = ImageTk.PhotoImage(image=Image.fromarray(nearest_im))
+                txt1 = "classified: " + str(nearest_id)
+                label_nearest = ttk.Label(
+                    master=tmp_canvas,
+                    image=nearest_im,
+                    text=txt1,
+                    compound="top"
+                )
+                label_nearest.image = nearest_im
+                label_nearest.grid(row=row, column=col+1)
+                row += 1
+                col = 0
                 tmp_canvas_col += 1
-                tmp_canvas_row = 1
-            #if r % 9 == 0:
-            #    r = 1
-            #    c +=4
+                if tmp_canvas_col % 17 == 0:
+                    tmp_canvas_row += 1
+                    tmp_canvas_col = 1
 
-        # toimii mutta liian sekava
+        # toimii mutta liian sekava eikä näytä kaikkia
         #for ind in ppl[:25]:
         #    nearest_nbrs = ind.get_nearest_neighbor()
         #    test_item = nearest_nbrs[0]
